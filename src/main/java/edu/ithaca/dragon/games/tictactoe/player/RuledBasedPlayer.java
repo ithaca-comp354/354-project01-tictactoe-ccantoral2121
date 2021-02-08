9e7c1ff6d39d1e584164a23cee4bfc47aa430b85
Cristian Cantoral <cristianggerardo@hotmail.com>
2021-02-08 09:56:19 -0500

    AI v0.2

--- src/main/java/edu/ithaca/dragon/games/tictactoe/player/RuledBasedPlayer.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/player/RuledBasedPlayer.java
@@ -4,26 +4,42 @@ import java.util.stream.IntStream;
 
 import org.javatuples.Pair;
 
+import edu.ithaca.dragon.games.tictactoe.GameStatus;
 import edu.ithaca.dragon.games.tictactoe.board.TicTacToeBoard;
 
 public class RuledBasedPlayer implements TicTacToePlayer {
 
     @Override
     public Pair<Integer, Integer> chooseSquare(TicTacToeBoard curBoard, char yourSymbol) {
-     
+         
         
         //Rule #5
         if(curBoard.isSquareOpen(new Pair<>(1,1))){
             return new Pair<>(1,1);
             
-        }else{
-            for (int y=0; y<3; y++){
-                for(int x=0; x<3;x++){
-                    if (curBoard.isSquareOpen(new Pair<>(x,y))){
-                        return new Pair<>(x,y);
-                    }
+        }else if(curBoard.calcGameStatus() == GameStatus.PLAYING) {
+
+                if(curBoard.isSquareOpen(new Pair<>(0,0))){
+                    return new Pair<>(0,0);
+                }else if(curBoard.isSquareOpen(new Pair<>(0,1))){
+                    return new Pair<>(0,1);
                 }
-            }
+                else if(curBoard.isSquareOpen(new Pair<>(2,2))){
+                    return new Pair<>(2,2);
+                }else if(curBoard.isSquareOpen(new Pair<>(2,1))){
+                    return new Pair<>(2,1);
+
+                }else if(curBoard.isSquareOpen(new Pair<>(2,0))){
+                    return new Pair<>(2,0);
+                }else if(curBoard.isSquareOpen(new Pair<>(1,0))){
+                    return new Pair<>(1,0);
+                }
+                else if(curBoard.isSquareOpen(new Pair<>(0,2))){
+                    return new Pair<>(0,2);
+                }else{
+                    return new Pair<>(1,2);
+                }
+        
         }
         
         return null;
