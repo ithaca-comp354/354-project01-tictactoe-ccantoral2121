3ba32d825b6a7a08d6995c02296cd2957ef2468c
Cristian Cantoral <cristianggerardo@hotmail.com>
2021-02-09 18:32:31 -0500

    AI V3

--- src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
@@ -2,12 +2,14 @@ package edu.ithaca.dragon.games.tictactoe;
 
 import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
-import edu.ithaca.dragon.games.tictactoe.player.RuledBasedPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.RuleBasedAgent;
+import edu.ithaca.dragon.games.tictactoe.player.RuledBasedAgentCristian;
 
 public class TicTacToeMain {
     public static void main (String[] args){
         //new TicTacToeGame( new HumanPlayer(), new BrainDeadPlayer()).play();
-        new TicTacToeGame( new HumanPlayer(), new RuledBasedPlayer()).play();
+        new TicTacToeGame( new RuleBasedAgent(), new RuledBasedAgentCristian()).play();
+
     }
     
 }
